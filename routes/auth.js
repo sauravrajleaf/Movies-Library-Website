bb50c454dbf72fe7156bf342c09362fc5670842c
sauravrajleaf <saurved785@gmail.com>
2022-05-13 20:39:37 +0530

    finished setting up login & signup backend

---
+++ routes/auth.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const config = require("config");
+const auth = require("../middleware/auth");
+
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
+// @route GET api/auth
+// @get logged in user
+// @access Private
+router.get("/", auth, async (req, res) => {
+	// res.send("Get logged in user");
+	try {
+		const user = await User.findById(req.user.id).select("-password");
+		res.json(user);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send("Server Error");
+	}
+});
+
+// @route POST api/auth
+// @auth user and get the jwt token
+// @access Public
+router.post(
+	"/",
+	[
+		body("email", "Please include a valid email").isEmail(),
+		body("password", "Password is required").exists(),
+	],
+	async (req, res) => {
+		// res.send("Log in user");
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			if (!user) {
+				return res.send(400).json({ msg: "Invalid Credentials" });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{
+					expiresIn: 360000000,
+				},
+				(error, token) => {
+					if (error) throw error;
+					res.json({ token });
+				}
+			);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send("Server Error");
+		}
+	}
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get('/', (req,res) => res.send('hey there'));
+connectDB();
 
-const PORT = process.env.PORT || 5000 ; 
+// initialising middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("hey there"));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/mylist", require("./routes/mylist"));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
